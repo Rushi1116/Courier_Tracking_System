391e6c562f03d4a1528e02ddf9c76191f07fe14e
Rushi1116 <118335819+Rushi1116@users.noreply.github.com>
2023-01-09 16:53:00 +0530

    employee

---
+++ HugsForBugs/Employee.java
@@ -0,0 +1,29 @@
+package com.dailycodebuffer.ProductService.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long productId;
+
+    @Column(name = "PRODUCT_NAME")
+    public String productName;
+
+    @Column(name = "ADDRESS")
+    public String address;
+
+    @Column(name = "AGENT_NAME")
+    public String AgentName;
+}
