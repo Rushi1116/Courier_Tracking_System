8ab96412205c854b031e8acec29ddbe37582618a
kaunainsaifi <118335538+kaunainsaifi@users.noreply.github.com>
2023-01-09 16:55:38 +0530

    model

---
+++ HugsForBugs/ProductRequest.java
@@ -0,0 +1,11 @@
+package com.dailycodebuffer.ProductService.model;
+
+import lombok.Data;
+
+@Data
+public class ProductRequest {
+    private String productName;
+    private String address;
+    private String AGENT_NAME;
+   
+}
